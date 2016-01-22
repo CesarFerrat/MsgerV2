8774523f4df10465a7b513644532f1e2c43c2ece
Cesar Ferrat <cesar.ferrat1@gmail.com>
2016-01-22 14:07:40 -0800

    create function:  picks only relavant datas from json file, add them to a new Hash and then store it into the database

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,19 @@
 class MessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-
   def create
+      new_message = Hash.new
+
+      new_message[:message_id] = params['id']
+      new_message[:from] = params['payload']['headers'][2]['value']
+      new_message[:to] = params['payload']['headers'][3]['value']
+      new_message[:date] = params['payload']['headers'][1]['value']
+      new_message[:subject] = params['payload']['headers'][5]['value']
+      new_message[:body] = params['payload']['parts'][0]['body']['data']
+
+      final_message = Message.create(new_message)
+
+      render :json => new_message, status: 201
   end
 
   def show
